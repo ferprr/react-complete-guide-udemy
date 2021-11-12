3f0cde49b30f818e8c040e94fc0e797325a77986
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-11-12 13:00:43 -0300

    Creating the first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 264.67,
+      date: new Date(2020, 7, 25),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 500.0,
+      date: new Date(2020, 7, 7),
+    },
+    {
+      id: "e4",
+      title: "Rent",
+      amount: 400.0,
+      date: new Date(2020, 8, 1),
+    }
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
