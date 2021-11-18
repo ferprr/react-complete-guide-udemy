3bdeaf6c56fce2a43e856763ce11b1e2e61a640e
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-11-18 14:55:13 -0300

    Splitting components into multiple components

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       title: "Rent",
       amount: 400.0,
       date: new Date(2020, 8, 1),
-    }
+    },
   ];
 
   return (
@@ -35,22 +35,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
